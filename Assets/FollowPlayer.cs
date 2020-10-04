5a12aa461ec4fe07cb25b37259e0cc50cb5eed46
AlexanderPecl <alexander.pecl@stud.fh-campuswien.ac.at>
2020-10-04 13:05:40 +0200

    First Commit * Icludes Runner Sprites and Basic Running script

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] int offset;
+    Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = transform.position;
+
+        temp.x = playerTransform.position.x - offset;
+
+        transform.position = temp;
+
+    }
+}
