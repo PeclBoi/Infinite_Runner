ad81f362d651238f8c252fced739d3bef4866363
PeclBoi <alexander.pecl@gmx.at>
2020-12-23 21:08:30 +0100

    Level wird unendlich geladen aber immer nur der selbe Teil

--- Assets/Script/LevelGenerator.cs
+++ Assets/Script/LevelGenerator.cs
@@ -4,17 +4,42 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    [SerializeField] private Transform initPart;
     [SerializeField] private Transform coursePart_1;
+    //[SerializeField] private GameObject player;
+
+    private const float PLAYER_DISTANCE_SPAWN_LEVEL_PART = 100f;
+    private Vector3 lastEndPosition;
 
     private void Awake()
     {
-        SpawnLevelPart(new Vector3(5, -5));
-        SpawnLevelPart(new Vector3(5, -5) + new Vector3(20, 0));
-        SpawnLevelPart(new Vector3(5, -5) + new Vector3(20 + 20, 0));
+        lastEndPosition = initPart.Find("EndPosition").position;
+        SpawnLevelPart();
     }
 
-    private void SpawnLevelPart(Vector3 spawnPosition)
+    private void Update()
     {
-        Instantiate(coursePart_1, spawnPosition, Quaternion.identity);
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        print(Vector3.Distance(player.transform.position, lastEndPosition));
+        print(player.transform.position);
+        if(Vector3.Distance(player.transform.position, lastEndPosition) < PLAYER_DISTANCE_SPAWN_LEVEL_PART)
+        {
+            //spawn another Level Part
+            SpawnLevelPart();
+            SpawnLevelPart();
+            SpawnLevelPart();
+        }
+    }
+
+    private void SpawnLevelPart()
+    {
+        Transform lastLevelPartTransform = SpawnLevelPart(lastEndPosition);
+        lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
+    }
+    private Transform SpawnLevelPart(Vector3 spawnPosition)
+    {
+        Transform levelPartTransform = Instantiate(coursePart_1, spawnPosition, Quaternion.identity);
+        print(levelPartTransform);
+        return levelPartTransform;
     }
 }
