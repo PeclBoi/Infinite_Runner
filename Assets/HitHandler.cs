0e3bf65334aa4802ca932bdb20bf659be6e997a6
PeclBoi <alexander.pecl@gmx.at>
2021-01-03 12:27:46 +0100

    Runner can shoot * Objects with enemy tag will be destroyed

---
+++ Assets/HitHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitHandler : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Enemy")
+        {
+            Destroy(collision.collider.gameObject);
+            Destroy(gameObject);
+        } 
+    }
+}
