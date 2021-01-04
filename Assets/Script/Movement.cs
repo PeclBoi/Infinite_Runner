5b2ad0dedcdc6038ee976959707568665a8fc675
Max <mehlfuerermax98@outlook.com>
2021-01-04 12:19:31 +0100

    Pausemenu btns assets done, score is shown but needs to be reajustet, jumpbtn bug with endless jumps fixed. if jumpbtn pressed only one jump

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Movement : CollisionMannager
 {
@@ -14,13 +15,17 @@ public class Movement : CollisionMannager
     [SerializeField] private int speed;
     [SerializeField] private float jumpVelocity = 100f;
     
+    
     private Rigidbody2D rigidbody2d;
     private float JumpHight;
     private float maxJumpHight = 3;
+    private bool click = false;
 
     void Start()
     {
+
         rigidbody2d = transform.GetComponent<Rigidbody2D>();
+        
     }
 
     void Update()
@@ -30,7 +35,7 @@ public class Movement : CollisionMannager
 
     private void CheckMovement()
     {
-        bool jumpIsPressedWhileOnGround = Input.GetKeyDown(KeyCode.Space) && isOnGround();
+        bool jumpIsPressedWhileOnGround = (Input.GetKeyDown(KeyCode.Space)|| click==true) && isOnGround();
         if (jumpIsPressedWhileOnGround)
         {
             Jump();
@@ -39,11 +44,11 @@ public class Movement : CollisionMannager
         {
             rigidbody2d.velocity = new Vector2(speed, 0);
         }
-
+        click = false;
         AttenuateJumpAtMaxHight();
     }
 
-    public void Jump()
+    private void Jump()
     {
         JumpHight = transform.position.y;
         rigidbody2d.AddForce(new Vector2(0, jumpVelocity));
@@ -57,6 +62,10 @@ public class Movement : CollisionMannager
         }
     }
     
-
+    public void Jumpbtnclicked()
+    {
+        click = true;
+        CheckMovement();
+    }
     
 }
